911ee5cab141d1a3aaa757cc8ec30a5b312a59ad
ButA1 <alexanderbut97@gmail.com>
2024-01-20 18:46:30 +0100

    legbot is chasing player

--- Unidot_test/Assets/Prefabs/Enemies/EnemyNavMesh.gd
+++ Unidot_test/Assets/Prefabs/Enemies/EnemyNavMesh.gd
@@ -2,12 +2,13 @@ extends CharacterBody3D
 
 # values taken from Unity navmesh agent
 @export var speed = 5
+var current_speed = 0
 @export var accel = 40
 @export var angular_speed = 900
-@export var DistanceFromTargetToStop = 1 # may need adjustment from the unity value
+@export var DistanceFromTargetToStop = 6 # may need adjustment from the unity value
 @onready var nav:NavigationAgent3D = $NavigationAgent3D
 @onready var animationTree = $LegsBot/AnimationTree
-
+var positionToGo
 
 func _ready():
 	StopNavigationAgent()
@@ -16,13 +17,16 @@ func _on_legs_bot_enable_navmesh():
 	print("turned on navmesh")
 	if is_instance_valid(nav):
 		animationTree.changeStateWalking()
-		nav.target_position = Vector3(0,0,16)
+		current_speed = speed
+		positionToGo = get_node("/root/player")
 
 
 func _physics_process(delta):
+	if is_instance_valid(positionToGo):
+		nav.target_position = positionToGo.position
 	var next = nav.get_next_path_position()
 	var current = global_position
-	var new_velocity = current.direction_to(next)*speed
+	var new_velocity = current.direction_to(next)*current_speed
 	# in Godot you need to implement your own movement
 	# some stuff from a youtube video, I might need to change it
 	velocity = velocity.move_toward(new_velocity,accel) 
@@ -32,8 +36,12 @@ func _physics_process(delta):
 	move_and_slide()
 	if(nav.distance_to_target() < DistanceFromTargetToStop):
 		StopNavigationAgent()
+	else:
+		animationTree.changeStateWalking()
+		current_speed = speed
 	
 func StopNavigationAgent():
 	animationTree.changeStateNotWalking()
-	nav.target_position = global_position
+	#nav.target_position = global_position
+	current_speed = 0
 
